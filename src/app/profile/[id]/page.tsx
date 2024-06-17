962195a86d9e04afc6262707d25dc7dd2a93c3bd
Nithya1234567890 <nithyamunagapati@gmai.com>
2024-06-17 17:04:00 +0530

    changes made

--- src/app/profile/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function userProfile({params}: any){
-    return(
-        <div className="flex flex-col justify-center gap-8 items-center min-h-screen">
-            <h1 className="text-5xl">Profile</h1>
-            <p className="text-3xl">Profile Page <span className=" bg-orange-400 text-white p-3 rounded-md border border-black"> {params.id}</span></p>
-        </div>
-    )
-}
